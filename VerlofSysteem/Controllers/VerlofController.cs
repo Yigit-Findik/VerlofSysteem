6413e4670b109ce84844bcade5c359412fb8fba0
Yigit Findik <yigit8@live.nl>
2023-04-19 09:36:43 +0200

    Initial commit

---
+++ VerlofSysteem/Controllers/VerlofController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using VerlofSysteem.Models;
+
+namespace VerlofSysteem.Controllers
+{
+    public class VerlofController : Controller
+    {
+        public IActionResult Index()
+        {
+
+            var verloven = new List<Verlof>
+            {
+                new Verlof { Id = 1, Gebruiker = "Johan", AanvraagDatum = DateTime.Now, StartDatum = new DateTime(2023, 4, 19), EindDatum = new DateTime(2023, 5, 19) },
+                new Verlof { Id = 2, Gebruiker = "Jacks", AanvraagDatum = DateTime.Now, StartDatum = new DateTime(2023, 5, 19), EindDatum = new DateTime(2023, 6, 19) },
+                new Verlof { Id = 3, Gebruiker = "Gabriel", AanvraagDatum = DateTime.Now, StartDatum = new DateTime(2023, 6, 19), EindDatum = new DateTime(2023, 7, 19) }
+            };
+            ViewData["verloven"] = verloven;
+            return View();
+        }
+    }
+}
---
+++ VerlofSysteem/Models/Verlof.cs
@@ -0,0 +1,11 @@
+﻿namespace VerlofSysteem.Models
+{
+    public class Verlof
+    {
+        public int Id { get; set; }
+        public string Gebruiker { get; set; }
+        public DateTime AanvraagDatum { get; set; }
+        public DateTime StartDatum { get; set; }
+        public DateTime EindDatum { get; set; }
+    }
+}
