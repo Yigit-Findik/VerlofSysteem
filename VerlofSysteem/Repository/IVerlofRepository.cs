e79da612796f8c9806d03aea9dfc3b2836d6c60d
Yigit Findik <yigit8@live.nl>
2023-04-25 10:14:54 +0200

    Repository Injection

---
+++ VerlofSysteem/Repository/IVerlofRepository.cs
@@ -0,0 +1,16 @@
+﻿using VerlofSysteem.Models;
+
+namespace VerlofSysteem.Repository;
+
+public interface IVerlofRepository
+{
+    Verlof Add(Verlof verlof);
+    
+    Verlof Update(Verlof verlof);
+    
+    Verlof Delete(int Id);
+    
+    Verlof GetVerlof(int Id);
+    
+    IEnumerable<Verlof> GetAllVerlof();
+}
---
+++ VerlofSysteem/Repository/VerlofRepository.cs
@@ -0,0 +1,43 @@
+﻿using VerlofSysteem.Models;
+
+namespace VerlofSysteem.Repository;
+
+public class VerlofRepository : IVerlofRepository
+{
+    private List<Verlof> _verlofs;
+
+    public VerlofRepository()
+    {
+        //Mock data
+        _verlofs = new List<Verlof>()
+        {
+            new Verlof(){ Id = 1, Gebruiker = "Jack", AanvraagDatum = DateTime.Now, StartDatum = DateTime.Now.AddDays(7), EindDatum = DateTime.Now.AddMonths(1) },
+            new Verlof(){ Id = 2, Gebruiker = "Jason", AanvraagDatum = DateTime.Now, StartDatum = DateTime.Now.AddDays(7), EindDatum = DateTime.Now.AddMonths(1) }
+        };
+    }
+    
+    public Verlof Add(Verlof verlof)
+    {
+        throw new NotImplementedException();
+    }
+    
+    public Verlof Delete(int Id)
+    {
+        throw new NotImplementedException();
+    }
+    
+    public IEnumerable<Verlof> GetAllVerlof()
+    {
+        return _verlofs;
+    }
+    
+    public Verlof GetVerlof(int Id)
+    {
+        throw new NotImplementedException();
+    }
+    
+    public Verlof Update(Verlof verlof)
+    {
+        throw new NotImplementedException();
+    }
+}
